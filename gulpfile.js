30ad2f3b821a57b80ef3474daee9a7d3c31621e4
Richard Franklin <richard.franklin@designagency.co.uk>
2017-04-05 09:35:59 +0100

    Initial project commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+﻿/// <binding AfterBuild='default' Clean='clean' />
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var del = require('del');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('sass/styles.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.scss', ['styles']);
+});
